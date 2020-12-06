19f9e39bbe815883b4e33698f5ef70c30d15cf46
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2020-12-06 23:57:51 +0300

    Init problem

---
+++ Problems/Creating a map/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Map <String, Integer> map = Map.of("Alpha", 1, "Gamma", 3, "Omega", 24);
+
+        System.out.println(map);
+    }
+}
